4b2c9ab5c9cbac6ca78463d7d8d4c182e28543fd
Zachary Tyler <zack.tyler.938@gmail.com>
2019-07-04 11:55:29 -0600

    Added Shape class.

--- script.js
+++ script.js
@@ -12,8 +12,17 @@ resize();
 
 class Vec2 {
     constructor(x, y) {
+        this.set(x, y);
+    }
+
+    set(x, y) {
         this.x = x;
         this.y = y;
+    }
+
+    set(v) {
+        this.x = v.x;
+        this.y = v.y;
     }
 
     addVec(v) {
@@ -32,6 +41,107 @@ class Vec2 {
     static cross(a, b) {
         return a.x * b.y - a.y * b.x;
     }
+
+    static newArray(length) {
+        let arr = [];
+        for (let i = 0; i < length; ++i) {
+            arr.push(new Vec2(0, 0));
+        }
+        return arr;
+    }
+}
+
+class Shape {
+    constructor(params) {
+        this.type = params.type;
+        this.radius = 0;
+        //this.body;
+        //this.u = new Mat2();
+        if (this.type === 'c') {
+            this.radius = params.radius;
+        }
+        else if (this.type === 'p') {
+            this.vertices = Vec2.newArray(8);
+            this.normals = Vec2.newArray(8);
+            this.vertexCount = 0;
+            if (params.hw) {
+                vertexCount = 4;
+                vertices[0].set(-params.hw, -params.hh);
+                vertices[1].set(params.hw, -params.hh);
+                vertices[2].set(params.hw, params.hh);
+                vertices[3].set(-params.hw, params.hh);
+                normals[0].set(0, -1);
+                normals[1].set(1, 0);
+                normals[2].set(0, 1);
+                normals[3].set(-1, 0);
+            }
+            /*else {
+                let verts = params.vertices;
+                // Find the right most point on the hull
+                let rightMost = 0;
+                let highestX = verts[0].x;
+                for (let i = 1; i < verts.length; ++i) {
+                    let x = verts[i].x;
+                    if (x > highestX) {
+                        highestX = x;
+                        rightMost = i;
+                    }
+                    // If same x, take lowest y
+                    else if (x == highestX) {
+                        if (verts[i].y < verts[rightMost].y) {
+                            rightMost = i;
+                        }
+                    }
+                }
+
+                let hull = new Array(8);
+                let outCount = 0;
+                let indexHull = rightMost;
+
+                for (;;) {
+                    hull[outCount] = indexHull;
+
+                    // Find next index that wraps around hull (find the next most counter-clockwise vertex)
+                    let nextHullIndex = 0;
+                    for (let i = 1; i < verts.length; ++i) {
+                        // Skip if the same coord
+                        if (nextHullIndex = indexHull) {
+                            nextHullIndex = i;
+                            continue;
+                        }
+
+                        // Cross every set of three unique verts
+                    }
+                }
+            }*/
+        }
+    }
+    
+    clone() {
+        if (this.type === 'c') {
+            return new Shape({
+                type: 'c',
+                radius: this.radius
+            });
+        }
+        else if (this.type === 'p') {
+            let s = new Shape({
+                type: 'p'
+            });
+            //s.u.set(this.u);
+            for (let i = 0; i < this.vertexCount; ++i) {
+                s.vertices[i].set(this.vertices[i]);
+                s.normals[i].set(this.normals[i]);
+            }
+            s.vertexCount = this.vertexCount;
+            return s;
+        }
+    }
+
+    initialize() {}
+    computeMass(density) {}
+    setOrient(radians) {}
+    getType() {}
 }
 
 class Body {
